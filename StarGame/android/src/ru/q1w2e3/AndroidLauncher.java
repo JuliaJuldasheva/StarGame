20a284c7a3911f6166cebc9845bc71ff602a48ec
JuliaJuldasheva <julia.vertegel@gmail.com>
2019-10-31 18:07:49 +0700

    All

---
+++ StarGame/android/src/ru/q1w2e3/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.q1w2e3;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.q1w2e3.StarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}
+}
